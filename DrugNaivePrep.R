f8c36a3d50ad050737afa9d8e8b0784d1b51c204
Sandra Ng <s.ng@qmul.ac.uk>
2023-03-30 15:10:31 +0100

    update R file

--- DrugNaivePrep.R
+++ DrugNaivePrep.R
@@ -25,3 +25,22 @@ edata <- edata[, clin$SAMPLE.ID]
 
 write.csv(clin, "output/clin_drugnaive.csv", row.names=FALSE)
 write.csv(edata, "output/edata_drugnaive.csv", row.names=TRUE)
+
+# normalised data
+edata_probes_norm <- fread("data/gene.expression.matrix_drug.naive_age.rin.removed.txt", 
+                      header=TRUE) %>%
+  mutate(ProbeID = sub(".*:","",ProbeID)) %>%
+  dplyr::rename("SYMBOL" = "ProbeID")
+
+edata_norm <- avereps(edata_probes_norm, ID = edata_probes_norm$SYMBOL)
+
+symbols_norm <- edata_norm[,1]
+rownames(edata_norm) <- symbols_norm
+edata_norm <- edata_norm[, -1]
+
+edata_norm <- edata_norm[, clin$SAMPLE.ID]
+
+write.csv(clin, "output/clin_drugnaive_norm.csv", row.names=FALSE)
+write.csv(edata_norm, "output/edata_drugnaive_norm.csv", row.names=TRUE)
+
+
